ed65fa196dc41f6e4198853f62645a2a4c1988c7
Elena <87865718+ElenaDelLla@users.noreply.github.com>
2022-07-22 14:49:20 +0200

    Update ft_strcpy.c

--- C/C02/ex00/ft_strcpy.c
+++ C/C02/ex00/ft_strcpy.c
@@ -10,22 +10,27 @@
 /*                                                                            */
 /* ************************************************************************** */
 
+//#include<stdio.h>
+
 char	*ft_strcpy(char *dest, char *src)
 {
+    char *temp;
+    temp=dest;
 	while (*src != '\0')
 	{
 		*dest = *src;
 		src++;
 		dest++;
 	}
-	return (dest);
+	*dest = '\0';
+    return temp;
 }
+
 /*int    main(void)
 {
     char    dest[20]="";
     char    src[20]="Hola Marvin";
-
     printf("Cadena 1 %s y cadena 2 %s\n", dest, src);
-    ft_strcpy(dest, src);
-    printf("Cadena 1 %s y cadena 2 %s\n", dest, src);
+    printf("%s\n", ft_strcpy(dest, src));
+    //printf("Cadena 1 %s y cadena 2 %s\n", dest, src);
 }*/
