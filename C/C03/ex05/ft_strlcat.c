525b6e7781743f59a04334c5551f3a3f60471411
Elena <87865718+ElenaDelLla@users.noreply.github.com>
2022-07-24 12:12:33 +0200

    Update ft_strlcat.c

--- C/C03/ex05/ft_strlcat.c
+++ C/C03/ex05/ft_strlcat.c
@@ -12,38 +12,74 @@
 
 //#include<stdio.h>
 
+int string_length(char *string){
+    int length;
+    length = 0;
+    char	*ini;
+    ini = string;
+    while (*ini != '\0')
+    	{
+    	    *ini++;
+    		length++;
+    	}
+    return length;
+}
+
+void copy(char *dest, char *src, unsigned int size){
+    while (*src != '\0' && size > 1)
+    {
+        *dest = *src;
+        *dest++;
+        *src++;
+        size--;
+    }
+}
+
+char *goStringEnd(char *string){
+    while (*string != '\0')
+    {
+        string++;
+    }
+    return string;
+}
+
+char *closeIfNeeded(char *dest, char* src, unsigned int size){
+    if (size == 1 || *src == 0)
+    {
+        *dest = '\0';
+    }
+    return dest;
+}
+
 unsigned int	ft_strlcat(char *dest, char *src, unsigned int size)
 {
-	char	*ini;
-	int		c;
-
-	ini = dest;
-	while (*dest != '\0')
-	{
-		dest++;
-	}
-	c = 0;
-	while (*src != '\0' && c < size)
-	{
-		*dest = *src;
-		dest++;
-		src++;
-		c++;
-	}
-	*dest = '\0';
-	c = 0;
-	while (*ini != '\0')
-	{
-		ini++;
-		c++;
-	}
-	return (c);
+	int  len;
+    int  slen;
+    len = string_length(dest);
+    slen = 0;
+    if(size>0)
+    {
+        slen = string_length(src);
+        dest = goStringEnd(dest);
+        size = size - len;
+        while (*src != '\0' && size > 1)
+        {
+            *dest = *src;
+            *dest++;
+            *src++;
+            size--;
+        }
+        dest = closeIfNeeded(dest, src, size);
+    }
+    return (slen + len);
 }
+
 /*int main(void)
 {
-	char	dest[] = "hola";
-	char	src[] = "adios";
-	unsigned int	size = 3;
-
-	printf("%d", ft_strlcat(dest, src, size));
+	char	dest[] = "hol\0";
+	char	src[] = "adios\0";
+	unsigned int	size = 6;
+	printf("%d\n", ft_strlcat(dest, src, size));
+	printf("%s\n", dest);
+	printf("%s\n", src);
 }*/
